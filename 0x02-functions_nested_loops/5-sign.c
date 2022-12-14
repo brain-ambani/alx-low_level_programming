357613f6334e57dd21891eb67451d53edb7eb873
brian-ambani <brainambani1@gmail.com>
2022-12-14 06:38:14 -0800

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,21 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar ('+');/*putchar(43) apply wen using ASCII code */
+		putchar(',');
 		return (1);
+		putchar('\n');
 	}
 	else if (n == 0)
 	{
 		putchar ('0');/*putchar(48) apply when using ASCII code */
+		putchar(',');
 		return (0);
+		putchar('\n');
 	}
 	else
 	{
 		putchar('-'); /*putchar(45) apply this when using  ASCII code*/
+		putchar(',');
 		return (-1);
 	}
 }
