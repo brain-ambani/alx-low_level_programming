3eb583928112d2d65c2190a70747f19cf93dccab
brian-ambani <brainambani1@gmail.com>
2022-12-14 03:14:56 -0800

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - prints _putchar.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
