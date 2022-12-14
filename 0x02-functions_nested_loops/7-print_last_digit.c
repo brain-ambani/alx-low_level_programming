f8a668cbc5b8dff5de4585731b05c2edcca75305
brian-ambani <brainambani1@gmail.com>
2022-12-14 09:40:43 -0800

    prints the last digit of a number.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,30 +1,27 @@
 #include "main.h"
+#include "holberton.h"
 
 /**
- * print_sign - Determines if the input number
- * greater, iqual or less than zero.
+ * print_last_digit - Prints the last digit
+ * of a number.
  *
- * @n: The input number as an integer.
+ * @n: input number as an integer.
  *
- * Return: 1 is greater than zero. 0 is zero.
- * -1 is less than zero.
+ * Return: last digit.
  */
-int print_sign(int n)
+int print_last_digit(int n)
 {
-	if (n > 0)
+	int l;
+
+	l = n % 10;
+	if (l < 0)
 	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
+		_putchar(-l + 48);
+		return (-l);
 	}
 	else
 	{
-		_putchar(48);
-		return (0);
+		_putchar(l + 48);
+		return (l);
 	}
-	_putchar('\n');
 }
