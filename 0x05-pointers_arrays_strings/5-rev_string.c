aa29e8ed3b434c517e2527c0cda295f285b9f57a
brian-ambani <brainambani1@gmail.com>
2022-12-19 04:45:49 -0800

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ * Return: reversed string
+ */
+void rev_string(char *s)
+{
+	int len = 0, i, j;
+	char *str, temp;
+
+	while (count >= 0)
+	{
+		if (s[len] == '\0')
+			break
+		len++
+	}
+	str = s;
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,5 @@ void swap_int(int *a, int *b);
 void _puts(char *str);
 int _putchar(char);
 void print_rev(char *s);
+void rev_string(char *s);
 #endif
