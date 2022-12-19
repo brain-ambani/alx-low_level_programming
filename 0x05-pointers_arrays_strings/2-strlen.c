45bd0ebdceff705bcf166debeb33dd9f11f7ea5a
brian-ambani <brainambani1@gmail.com>
2022-12-19 04:06:37 -0800

    length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,13 @@
  * @s:input string
  * Return: length of string
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int len = 0;
 
 	if (*(s + len) != '\0')
+	{
 		len++;
+	}
 	return (len);
 }
