6a6ff21c912b15a6a0eb462729b445ce4f5744cf
brian-ambani <brainambani1@gmail.com>
2023-01-30 11:24:43 -0800

    updated task-0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,10 +16,10 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printtf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 
 		nelement++;
 	}
-	return (element);
+	return (nelement);
 }
