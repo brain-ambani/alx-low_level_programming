9159b6a26aa3613b0a02ab9c9e8d186ba5a763e9
brian-ambani <brainambani1@gmail.com>
2023-01-30 13:32:23 -0800

    updated task-100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * __attribute__ - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
