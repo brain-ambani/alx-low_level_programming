196538115573563a0f358588d50d6479eafd560e
brian-ambani <brainambani1@gmail.com>
2023-02-02 06:32:35 -0800

    updated task-1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t nnode = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		nnodes++;
+		nnode++;
 	}
-	return (nnodes);
+	return (nnode);
 }
