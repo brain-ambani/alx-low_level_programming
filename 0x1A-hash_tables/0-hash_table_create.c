789341be5119fc6731b0d07f68deed9f6129e006
brian-ambani <brainambani1@gmail.com>
2023-04-06 00:52:53 -0700

    working on task-o

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hashtable.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table
+ *
+ * Return: hash table
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	// some code
+}
