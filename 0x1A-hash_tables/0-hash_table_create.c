a220aad395a726d60cd61982387880488bad532e
brian-ambani <brainambani1@gmail.com>
2023-04-06 15:56:35 -0700

    updated task-0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-#include "hashtable.h"
+#include "hash_tables.h"
 
 /**
  * hash_table_create - creates a hash table
@@ -10,5 +10,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	// some code
+	hash_table_t *tbl;
+	hash_node_t **arr;
+	unsigned long int n;
+
+	tbl = malloc(sizeof(hash_table_t));
+	if (tbl == NULL)
+		return (NULL);
+
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (n = 0; n < size; n++)
+		arr[n] = NULL;
+
+	tbl->array = arr;
+	tbl->size = size;
+
+	return (tbl);
 }
